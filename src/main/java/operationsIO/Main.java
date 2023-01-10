1b6d3a9c5b4d5a7fa61ecfcabfc99987ec47970c
Arkadiusz Gwizdal <ark.gwizdal@gmail.com>
2023-01-10 18:46:24 +0100

    Commit path

--- src/main/java/operationsIO/Main.java
+++ src/main/java/operationsIO/Main.java
@@ -1,16 +1,42 @@
 package operationsIO;
 
 import java.io.*;
+import java.nio.CharBuffer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        bufferedReader();
+        path();
+//        buffer();
+//        bufferedReader();
 //        fileReader();
 //        fileInputStream();
 
     }
 
+    private static void path() throws IOException {
+        Path path = Paths.get("src/main/resources/chanelFile");
+        if (!Files.exists(path)) {
+            Files.createFile(path);
+        }
+        Files.write(path,"Tomek".getBytes(), StandardOpenOption.WRITE);
+    }
+    private static void buffer() {
+        CharBuffer charbuffer = CharBuffer.allocate(6);
+        String name = "Tomek";
+
+        for (int i =0; i<name.length(); i++){
+            char c = name.charAt(i);
+            charbuffer.put(c);
+        }
+//        System.out.println(charbuffer.array());
+        System.out.println(Arrays.toString(charbuffer.array()));
+    }
     private static void bufferedReader() throws IOException { //Przykład dekoratora, dekoratorem jest Buffereadreder dla Redera
         var fileReader = new FileReader("src/main/resources/input");
         var fileWriter = new FileWriter("src/main/resources/output");
